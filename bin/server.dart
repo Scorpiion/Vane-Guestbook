71b2255df48d9d150253467ff9aa1c82732400e4
Scorpiion <Robert@dartvoid.com>
2017-05-18 00:49:19 +0200

    Save

--- bin/server.dart
+++ bin/server.dart
@@ -1,7 +1,7 @@
 library server;
 
 import 'dart:async';
-import 'package:vane/vane.dart';
+import 'package:vane/vane_server.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
 part 'guestbook.dart';
